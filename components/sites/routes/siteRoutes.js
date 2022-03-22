55e4645fa946f0b1d263c885214cce3cfb468e38
karira <43937812+hackster254@users.noreply.github.com>
2022-03-22 10:02:30 +0300

    Add files via upload

--- components/sites/routes/siteRoutes.js
+++ components/sites/routes/siteRoutes.js
@@ -8,11 +8,12 @@ const { protect, checkAdmin } = require("../../users/middlewares/authMiddleware"
 
 const router = express.Router();
 
-// create a site
+// get a site for a specific user
 router.route("/sites").get(protect, getSites);
-
+// create a site
 router.route("/sites/create").post(protect, CreateSite)
 
+// get all sites // this is for admin
 router.route("/sites/all").get(protect, checkAdmin, getAllSites)
 
 
