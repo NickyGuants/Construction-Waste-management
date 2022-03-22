b466de44232edbc31b2df2c94d88b7bdb6b30c70
NickyGuants <nicholasguantai528@gmail.com>
2022-03-22 13:18:45 +0300

    fix routes

--- components/sites/routes/siteRoutes.js
+++ components/sites/routes/siteRoutes.js
@@ -9,12 +9,12 @@ const { protect, checkAdmin } = require("../../users/middlewares/authMiddleware"
 const router = express.Router();
 
 // get a site for a specific user
-router.route("/sites").get(protect, getSites);
+router.route("/").get(protect, getSites);
 // create a site
-router.route("/sites/create").post(protect, CreateSite)
+router.route("/create").post(protect, CreateSite)
 
 // get all sites // this is for admin
-router.route("/sites/all").get(protect, checkAdmin, getAllSites)
+router.route("/all").get(protect, checkAdmin, getAllSites)
 
 
 module.exports = router;
