96f18eb9c464a135012eedcd72f5c802fd24b437
karira <43937812+hackster254@users.noreply.github.com>
2022-03-22 10:14:01 +0300

    Add files via upload

--- components/users/routes/userRoutes.js
+++ components/users/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { registerUser, authUser } = require('../controllers/userControllers')
+const { registerUser, authUser, updateUserProfile } = require('../controllers/userControllers');
+const { protect, checkAdmin, } = require('../middlewares/authMiddleware');
 
 const router = express.Router()
 
@@ -9,6 +10,12 @@ router.route('/signup').post(registerUser)
 // login a user
 router.route("/login").post(authUser);
 
+// route to update users profile
+router.route("/profile").post(protect, updateUserProfile)
 
+
+router.route("/admin/login").post(protect, checkAdmin, authUser)
+
+// once logged in as admin you should be able to get all users/ all sites etc
 
 module.exports = router
