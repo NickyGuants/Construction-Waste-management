bfae420ddf532fb53e1e4a4e85f37fe06e5d4d02
karira <43937812+hackster254@users.noreply.github.com>
2022-03-24 16:10:00 +0300

    Added delete and update site routes

--- components/sites/controllers/siteController.js
+++ components/sites/controllers/siteController.js
@@ -41,6 +41,7 @@ const CreateSite = asyncHandler(async(req, res) => {
 const getAllSites = asyncHandler(async(req, res, next) => {
 
     const sites = await Site.find({})
+
     if (sites) {
         res.status(201).json({
             message: "Succes, All sites retrieved",
@@ -50,4 +51,75 @@ const getAllSites = asyncHandler(async(req, res, next) => {
 
 })
 
-module.exports = { getSites, CreateSite, getAllSites }
+const updateSite = asyncHandler(async(req, res, next) => {
+
+
+    const { id, sitename, desc, long, lat } = req.body
+
+
+    if (!id) {
+        res.status(400);
+        throw new Error("Please provide site id");
+        return;
+
+    }
+    if (!sitename, !desc, !long, !lat) {
+        res.status(400);
+        throw new Error("Please provide all site details");
+        return;
+
+    }
+
+
+    const findSite = await Site.findById(id);
+    if (findSite) {
+        // res.status(200).json({
+        //     message: `Success, specific site for id: ${req.body.id} found`,
+        //     site: findSite
+        // })
+        findSite.sitename = req.body.sitename || findSite.sitename
+        findSite.desc = req.body.desc || findSite.desc
+        findSite.long = long || findSite.long
+        findSite.lat = lat || findSite.lat;
+
+        const updatedSite = await findSite.save()
+
+        res.status(201).json({
+            success: "site updated successfully",
+            site: updatedSite
+
+        })
+    } else {
+        res.status(400).json({
+            "error": 'Specific site not found'
+        })
+    }
+
+})
+
+const deleteSite = asyncHandler(async(req, res, next) => {
+    try {
+        const { id } = req.body;
+
+        if (!id) {
+            res.status(400);
+            throw new Error("Please provide site id to delete");
+            return;
+        }
+
+        await Site.findByIdAndDelete(id);
+
+        res.status(201).json({
+            message: "Site successfully Deleted"
+        })
+
+    } catch (error) {
+        res.status(400);
+        throw new Error(error);
+        // return;
+
+    }
+
+})
+
+module.exports = { getSites, CreateSite, getAllSites, updateSite, deleteSite }
--- components/sites/routes/siteRoutes.js
+++ components/sites/routes/siteRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const {
     getSites,
     CreateSite,
-    getAllSites
+    getAllSites,
+    updateSite,
+    deleteSite
 } = require("../controllers/siteController");
 const { protect, checkAdmin } = require("../../users/middlewares/authMiddleware");
 
@@ -15,6 +17,12 @@ router.route("/sites/create").post(protect, CreateSite)
 
 // get all sites // this is for admin
 router.route("/sites/all").get(protect, checkAdmin, getAllSites)
+
+// route to update a users specific site details
+router.route("/sites/edit").post(protect, updateSite)
+
+// route to delete a users specific site details
+router.route("/sites/delete").post(protect, deleteSite)
 
 
 module.exports = router;
