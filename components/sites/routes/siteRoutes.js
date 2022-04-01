5daaba08008434c6555e3762eb7f9f3ae4d95595
NickyGuants <nicholasguantai528@gmail.com>
2022-04-01 16:21:47 +0300

    update routes

--- components/sites/routes/siteRoutes.js
+++ components/sites/routes/siteRoutes.js
@@ -13,27 +13,27 @@ const { protect, checkAdmin } = require("../../users/middlewares/authMiddleware"
 const router = express.Router();
 
 // get a site for a specific user
-router.route("/sites").get(protect, getSites);
+router.route("/").get(protect, getSites);
 // create a site
-router.route("/sites/create").post(protect, CreateSite)
+router.route("/create").post(protect, CreateSite)
 
 // get all sites // this is for admin
-router.route("/sites/all").get(protect, checkAdmin, getAllSites)
+router.route("/all").get(protect, checkAdmin, getAllSites)
 
 // route to get all sites, just for test ***
-router.route('/sites/every').get(getEverySite)
+router.route('/every').get(getEverySite)
 
 // route to get specific site
-router.route('/sites/:id').get(getSpecificSite)
+router.route('/:id').get(getSpecificSite)
 
 // route to update a users specific site details
 //router.route("/sites/edit/:id").post(protect, updateSite)
-router.route("/sites/edit/:id").post(updateSite);
+router.route("/edit/:id").post(updateSite);
 
 // route to delete a users specific site details
 // router.route("/sites/delete").post(protect, deleteSite)
 // // route to delete a users specific site details
-router.route("/sites/delete/:id").post(deleteSite)
+router.route("/delete/:id").post(deleteSite)
 
 
 module.exports = router;
